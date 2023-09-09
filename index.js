859951a1e904a452def0cbd306d33d1f364087f1
Alexandria Webb <alexandriakwebb@gmail.com>
2023-09-09 14:05:27 -0600

    passes all tests.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (array, query) {
+    return array.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+
+function fuzzyMatch (array, query) {
+    return array.filter((element) => element[0] === query[0])
+}
+
+function matchName (array, query) {
+    let driverObject
+    if (driverObject = array.filter((element) => element.name === query)) {
+        return driverObject
+    }
+}
